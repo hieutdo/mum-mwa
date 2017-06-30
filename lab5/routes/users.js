bfc2a89f01bcb19a1282d0a9e4ec4be51b0275ea
Hieu Do <mr.hieudt@gmail.com>
2017-06-30 15:03:14 -0500

    [lab5] Create /users/1 route to use promise

--- lab5/routes/users.js
+++ lab5/routes/users.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const fetch = require('node-fetch');
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/1', function (req, res, next) {
+  const model = { title: 'Users - Promise' };
+
+  fetch('http://jsonplaceholder.typicode.com/users/')
+    .then((res) => res.json())
+    .then((users) => {
+      model.users = users;
+      res.render('users', model);
+    });
 });
 
 module.exports = router;
