30cfc8384d06bb47cddc3bfa55263f566cbbea77
Hieu Do <mr.hieudt@gmail.com>
2017-06-30 20:10:16 -0500

    [lab5] Create /users/2 route and use Rxjs to fetch data

--- lab5/routes/users.js
+++ lab5/routes/users.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const fetch = require('node-fetch');
+const Rx = require('@reactivex/rxjs');
 const router = express.Router();
 
+const apiUrl = 'http://jsonplaceholder.typicode.com/users/';
+
 /* GET users listing. */
-router.get('/1', function (req, res, next) {
+router.get('/1', (req, res) => {
   const model = { title: 'Users - Promise' };
 
-  fetch('http://jsonplaceholder.typicode.com/users/')
+  fetch(apiUrl)
     .then((res) => res.json())
     .then((users) => {
+      model.users = users;
+      res.render('users', model);
+    });
+});
+
+router.get('/2', (req, res) => {
+  const model = { title: 'Users - Observables' };
+
+  Rx.Observable.from(fetch(apiUrl))
+    .flatMap((res) => res.json())
+    .subscribe((users) => {
       model.users = users;
       res.render('users', model);
     });
