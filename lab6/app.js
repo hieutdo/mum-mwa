933423779d31388d6a881e944124550ff4136f25
Hieu Do <mr.hieudt@gmail.com>
2017-07-02 22:05:50 -0500

    [lab6] Create /newsletter and /thankyou route

--- lab6/app.js
+++ lab6/app.js
@@ -1,14 +1,16 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const validator = require('express-validator');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const index = require('./routes/index');
+const users = require('./routes/users');
+const newsletter = require('./routes/newsletter');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,21 +21,27 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/newsletter', newsletter);
+
+app.get('/thankyou', function (req, res) {
+  res.render('thankyou', { email: req.query.email });
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ lab6/routes/newsletter.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const querystring = require('querystring');
+const express = require('express');
+const router = express.Router();
+
+const dataFile = path.join(__dirname, '..', 'data', 'subscribers.txt');
+
+router.get('/', function (req, res) {
+  res.render('newsletter', { errors: null, email: '' });
+});
+
+router.post('/', function (req, res, next) {
+  req.assert('email', 'Email cannot be left empty.').notEmpty();
+  req.assert('email', 'A valid email is required.').isEmail();
+
+  const { email } = req.body;
+  const errors = req.validationErrors();
+
+  if (errors) {
+    res.render('newsletter', { email, errors });
+    return;
+  }
+
+  const stream = fs.createWriteStream(dataFile, { flags: 'a' });
+  stream.write(email + '\r\n');
+  stream.end();
+
+  res.redirect(`/thankyou?${querystring.stringify({ email })}`);
+});
+
+module.exports = router;
