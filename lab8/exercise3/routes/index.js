931a31d249a6bf608e9151145e4ca93d7d055545
Hieu Do <mr.hieudt@gmail.com>
2017-07-05 16:04:17 -0500

    [lab8] Create Location's list view

--- lab8/exercise3/routes/index.js
+++ lab8/exercise3/routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  db.getCollection('locations').then((collection) => {
+    collection.find({}).toArray((err, locations) => {
+      if (err) {
+        return next(err);
+      }
+      res.render('index', { title: 'Locations', locations });
+    });
+  });
 });
 
 module.exports = router;
