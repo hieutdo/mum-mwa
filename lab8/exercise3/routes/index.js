cec793ded32e581f2486feb83acc1a2d2c640d26
Hieu Do <mr.hieudt@gmail.com>
2017-07-06 11:39:46 -0500

    [lab8] Refactor code
    * use async/await to handle requests

--- lab8/exercise3/routes/index.js
+++ lab8/exercise3/routes/index.js
@@ -1,36 +1,35 @@
 const { ObjectID } = require('mongodb');
 const express = require('express');
 const router = express.Router();
-const db = require('../db');
 
-router.get('/', function (req, res, next) {
-  db.getCollection('locations').then((collection) => {
-    collection.find({}).toArray((err, locations) => {
-      if (err) {
-        return next(err);
-      }
-      res.render('index', { title: 'Locations', locations });
-    });
-  });
+router.get('/', async (req, res, next) => {
+  try {
+    const db = req.app.locals.db;
+    const col = db.collection('locations');
+    const locations = await col.find({}).toArray();
+    res.render('index', { title: 'Locations', locations });
+  } catch (e) {
+    next(e);
+  }
 });
 
-router.get('/edit/:id', function (req, res, next) {
-  const { id } = req.params;
-  const { saved } = req.query;
+router.get('/edit/:id', async (req, res, next) => {
+  try {
+    const db = req.app.locals.db;
+    const col = db.collection('locations');
+    const { id } = req.params;
+    const { saved } = req.query;
+    const location = await col.findOne({ _id: ObjectID(id) });
 
-  db.getCollection('locations').then((collection) => {
-    collection.findOne({ _id: ObjectID(id) }, (err, location) => {
-      if (err) {
-        return next(err);
-      }
-      res.render('edit', {
-        title: `Edit Location: ${location.name}`,
-        location,
-        errors: {},
-        saved,
-      });
+    res.render('edit', {
+      title: `Edit Location: ${location.name}`,
+      location,
+      errors: {},
+      saved,
     });
-  });
+  } catch (e) {
+    next(e);
+  }
 });
 
 router.get('/create', function (req, res) {
@@ -46,34 +45,33 @@ router.get('/create', function (req, res) {
   });
 });
 
-router.post(['/create', '/edit/:id'], function (req, res, next) {
-  const { body } = req;
-  const isCreate = !body._id;
+router.post(['/create', '/edit/:id'], async (req, res, next) => {
+  try {
+    const db = req.app.locals.db;
+    const col = db.collection('locations');
+    const { body } = req;
+    const isCreate = !body._id;
 
-  req.assert('name', 'Name is required').notEmpty();
-  req.assert('category', 'Category is required').notEmpty();
-  req.assert('coord.0', 'Longitude must be a valid number').isFloat();
-  req.assert('coord.1', 'Latitude must be a valid number').isFloat();
+    req.assert('name', 'Name is required').notEmpty();
+    req.assert('category', 'Category is required').notEmpty();
+    req.assert('coord.0', 'Longitude must be a valid number').isFloat();
+    req.assert('coord.1', 'Latitude must be a valid number').isFloat();
 
-  req.getValidationResult().then((validationResult) => {
+    const validationResult = await req.getValidationResult();
+
     if (validationResult.isEmpty()) {
-
       req.sanitize('coord.0').toFloat();
       req.sanitize('coord.1').toFloat();
 
-      db.getCollection('locations').then((collection) => {
-        if (isCreate) {
-          delete body._id;
-          collection.insertOne(body).then((result) => {
-            res.redirect('/edit/' + result.insertedId + '?saved=success');
-          });
-        } else {
-          body._id = ObjectID(body._id);
-          collection.findOneAndUpdate({ _id: body._id }, body).then((result) => {
-            res.redirect('/edit/' + result.value._id + '?saved=success');
-          });
-        }
-      });
+      if (isCreate) {
+        delete body._id;
+        const result = await col.insertOne(body);
+        res.redirect('/edit/' + result.insertedId + '?saved=success');
+      } else {
+        body._id = ObjectID(body._id);
+        const result = await col.findOneAndUpdate({ _id: body._id }, body);
+        res.redirect('/edit/' + result.value._id + '?saved=success');
+      }
     } else {
       res.render('edit', {
         title: isCreate ? 'Create New Location' : `Edit Location: ${body.name}`,
@@ -82,69 +80,70 @@ router.post(['/create', '/edit/:id'], function (req, res, next) {
         saved: 'fail',
       });
     }
-  });
+  } catch (e) {
+    next(e);
+  }
 });
 
-router.get('/search', function (req, res, next) {
-  const { query } = req;
-  const model = {
-    title: 'Search',
-    query,
-    errors: {},
-    searchResults: [],
-  };
+router.get('/search', async (req, res, next) => {
+  try {
+    const db = req.app.locals.db;
+    const col = db.collection('locations');
+    const { query } = req;
+    const model = {
+      title: 'Search',
+      query,
+      errors: {},
+      searchResults: [],
+    };
 
-  if (!model.query.maxDistance) {
-    model.query.maxDistance = 500;
-  }
+    if (!model.query.maxDistance) {
+      model.query.maxDistance = 500;
+    }
 
-  if (query.action) {
-    req.assert('category', 'Category is required').notEmpty();
+    if (query.action === 'submit') {
+      req.assert('category', 'Category is required').notEmpty();
 
-    req.getValidationResult().then((validationResult) => {
-      db.getCollection('locations').then((collection) => {
-        if (validationResult.isEmpty()) {
-          req.sanitize('maxDistance').toFloat();
-          req.sanitize('coord.0').toFloat();
-          req.sanitize('coord.1').toFloat();
+      const validationResult = await req.getValidationResult();
 
-          const filter = {
-            category: query.category,
+      if (validationResult.isEmpty()) {
+        req.sanitize('maxDistance').toFloat();
+        req.sanitize('coord.0').toFloat();
+        req.sanitize('coord.1').toFloat();
+
+        const filter = {
+          category: query.category,
+        };
+
+        if (query.name) {
+          filter.$text = {
+            $search: query.name,
           };
-
-          if (query.name) {
-            filter.$text = {
-              $search: query.name,
-            };
-          }
-
-          if (query.coord) {
-            filter.coord = {
-              $near: {
-                $geometry: {
-                  type: 'Point',
-                  coordinates: query.coord,
-                },
-                $maxDistance: query.maxDistance
-              },
-            };
-          }
-
-          collection.find(filter).toArray((err, results) => {
-            if (err) {
-              return next(err);
-            }
-            model.searchResults = results;
-            res.render('search', model);
-          });
-        } else {
-          model.errors = validationResult.mapped();
-          res.render('search', model);
         }
-      });
-    });
-  } else {
-    res.render('search', model);
+
+        if (query.coord) {
+          filter.coord = {
+            $near: {
+              $geometry: {
+                type: 'Point',
+                coordinates: query.coord,
+              },
+              $maxDistance: query.maxDistance,
+            },
+          };
+        }
+
+        model.searchResults = await col.find(filter).toArray();
+        res.render('search', model);
+      } else {
+        model.errors = validationResult.mapped();
+        res.render('search', model);
+      }
+    } else {
+      res.render('search', model);
+    }
+  } catch (e) {
+    next(e);
   }
 });
 
