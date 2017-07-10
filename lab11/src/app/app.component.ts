afface53ef72dce9b7ccb5205c12209224592bc3
Hieu Do <mr.hieudt@gmail.com>
2017-07-10 17:25:10 -0500

    [lab11] Listen to counterChange event and update AppComponent view

--- lab11/src/app/app.component.ts
+++ lab11/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  ComponentCounterValue = 1;
+
+  onCounterChange(value: number) {
+    this.ComponentCounterValue = value;
+  }
 }
