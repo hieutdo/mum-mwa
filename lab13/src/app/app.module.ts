b9812b710ded59e440dfc4513279dbd4ec24b725
Hieu Do <mr.hieudt@gmail.com>
2017-07-12 14:29:45 -0500

    [lab13] Create home component

--- lab13/src/app/app.module.ts
+++ lab13/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+
+const MY_ROUTES: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(MY_ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
