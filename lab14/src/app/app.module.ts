5ba1315e4a208d2ecc104c14a61ecbb66ea7e1e4
Hieu Do <mr.hieudt@gmail.com>
2017-07-13 14:58:06 -0500

    [lab14] Add validation to PostForm component

--- lab14/src/app/app.module.ts
+++ lab14/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PostFormComponent } from './post-form/post-form.component';
@@ -10,7 +11,8 @@ import { PostFormComponent } from './post-form/post-form.component';
     PostFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- lab14/src/app/post-form/post-form.component.ts
+++ lab14/src/app/post-form/post-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-post-form',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-form.component.css']
 })
 export class PostFormComponent implements OnInit {
+  postForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.postForm = fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      post: ['', [Validators.required, Validators.minLength(10)]]
+    });
+  }
 
   ngOnInit() {
+  }
+
+  handleSubmit() {
+    console.log(this.postForm.value);
   }
 
 }
