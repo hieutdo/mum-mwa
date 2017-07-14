001c3ab08195f5e8c635b1c6f8d60adcc865c920
Hieu Do <mr.hieudt@gmail.com>
2017-07-14 11:56:57 -0500

    [fer] Create Post component

---
+++ finalExerciseReview/src/app/post/post.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Post } from '../post';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+  @Input('data')
+  post: Post;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
