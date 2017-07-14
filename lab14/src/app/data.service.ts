4c6871a475fa4af61fa6f59b24c56b02ec2f4c4e
Hieu Do <mr.hieudt@gmail.com>
2017-07-13 20:15:01 -0500

    [lab14] Update some minor things

--- lab14/src/app/data.service.ts
+++ lab14/src/app/data.service.ts
@@ -4,14 +4,15 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class DataService {
+  private baseUrl = 'https://jsonplaceholder.typicode.com';
 
   constructor(private http: Http) { }
 
   getUser(id: number) {
-    return this.http.get(`http://jsonplaceholder.typicode.com/users/${id}`).map(res => res.json());
+    return this.http.get(`${this.baseUrl}/users/${id}`).map(res => res.json());
   }
 
   getPosts(userId: number) {
-    return this.http.get(`http://jsonplaceholder.typicode.com/posts?userId=${userId}`).map(res => res.json());
+    return this.http.get(`${this.baseUrl}/posts?userId=${userId}`).map(res => res.json());
   }
 }
